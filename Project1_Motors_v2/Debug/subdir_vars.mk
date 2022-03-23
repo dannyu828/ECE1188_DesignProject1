882643d730da05708d21ac7d7e59244836ef5730
dannyu828 <dau18@pitt.edu>
2022-03-23 00:44:07 -0400

    Added global vars and Motor_Receive()

---
+++ Project1_Motors_v2/Debug/subdir_vars.mk
@@ -0,0 +1,89 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p401r.cmd 
+
+C_SRCS += \
+C:/ti/tirslk_max_1_00_02/inc/Bump.c \
+C:/ti/tirslk_max_1_00_02/inc/Clock.c \
+C:/ti/tirslk_max_1_00_02/inc/CortexM.c \
+C:/ti/tirslk_max_1_00_02/inc/LaunchPad.c \
+C:/ti/tirslk_max_1_00_02/inc/Motor.c \
+C:/ti/tirslk_max_1_00_02/inc/PWM.c \
+../Project1_main.c \
+C:/ti/tirslk_max_1_00_02/inc/TimerA1.c \
+C:/ti/tirslk_max_1_00_02/inc/blinker.c \
+../startup_msp432p401r_ccs.c \
+../system_msp432p401r.c 
+
+C_DEPS += \
+./Bump.d \
+./Clock.d \
+./CortexM.d \
+./LaunchPad.d \
+./Motor.d \
+./PWM.d \
+./Project1_main.d \
+./TimerA1.d \
+./blinker.d \
+./startup_msp432p401r_ccs.d \
+./system_msp432p401r.d 
+
+OBJS += \
+./Bump.obj \
+./Clock.obj \
+./CortexM.obj \
+./LaunchPad.obj \
+./Motor.obj \
+./PWM.obj \
+./Project1_main.obj \
+./TimerA1.obj \
+./blinker.obj \
+./startup_msp432p401r_ccs.obj \
+./system_msp432p401r.obj 
+
+OBJS__QUOTED += \
+"Bump.obj" \
+"Clock.obj" \
+"CortexM.obj" \
+"LaunchPad.obj" \
+"Motor.obj" \
+"PWM.obj" \
+"Project1_main.obj" \
+"TimerA1.obj" \
+"blinker.obj" \
+"startup_msp432p401r_ccs.obj" \
+"system_msp432p401r.obj" 
+
+C_DEPS__QUOTED += \
+"Bump.d" \
+"Clock.d" \
+"CortexM.d" \
+"LaunchPad.d" \
+"Motor.d" \
+"PWM.d" \
+"Project1_main.d" \
+"TimerA1.d" \
+"blinker.d" \
+"startup_msp432p401r_ccs.d" \
+"system_msp432p401r.d" 
+
+C_SRCS__QUOTED += \
+"C:/ti/tirslk_max_1_00_02/inc/Bump.c" \
+"C:/ti/tirslk_max_1_00_02/inc/Clock.c" \
+"C:/ti/tirslk_max_1_00_02/inc/CortexM.c" \
+"C:/ti/tirslk_max_1_00_02/inc/LaunchPad.c" \
+"C:/ti/tirslk_max_1_00_02/inc/Motor.c" \
+"C:/ti/tirslk_max_1_00_02/inc/PWM.c" \
+"../Project1_main.c" \
+"C:/ti/tirslk_max_1_00_02/inc/TimerA1.c" \
+"C:/ti/tirslk_max_1_00_02/inc/blinker.c" \
+"../startup_msp432p401r_ccs.c" \
+"../system_msp432p401r.c" 
+
+
