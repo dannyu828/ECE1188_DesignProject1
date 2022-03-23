da50a578dc3dacb12aa3342784d5651ccc631868
dannyu828 <dau18@pitt.edu>
2022-03-22 23:05:25 -0400

    Initial upload

---
+++ Project1_Motors/Debug/subdir_vars.mk
@@ -0,0 +1,95 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p401r.cmd 
+
+C_SRCS += \
+C:/ti/tirslk_max_1_00_02/inc/Bump.c \
+C:/ti/tirslk_max_1_00_02/inc/Clock.c \
+C:/ti/tirslk_max_1_00_02/inc/CortexM.c \
+../Lab13_Timersmain.c \
+C:/ti/tirslk_max_1_00_02/inc/LaunchPad.c \
+C:/ti/tirslk_max_1_00_02/inc/Motor.c \
+C:/ti/tirslk_max_1_00_02/inc/PWM.c \
+C:/ti/tirslk_max_1_00_02/inc/TExaS.c \
+C:/ti/tirslk_max_1_00_02/inc/TimerA1.c \
+C:/ti/tirslk_max_1_00_02/inc/blinker.c \
+../startup_msp432p401r_ccs.c \
+../system_msp432p401r.c 
+
+C_DEPS += \
+./Bump.d \
+./Clock.d \
+./CortexM.d \
+./Lab13_Timersmain.d \
+./LaunchPad.d \
+./Motor.d \
+./PWM.d \
+./TExaS.d \
+./TimerA1.d \
+./blinker.d \
+./startup_msp432p401r_ccs.d \
+./system_msp432p401r.d 
+
+OBJS += \
+./Bump.obj \
+./Clock.obj \
+./CortexM.obj \
+./Lab13_Timersmain.obj \
+./LaunchPad.obj \
+./Motor.obj \
+./PWM.obj \
+./TExaS.obj \
+./TimerA1.obj \
+./blinker.obj \
+./startup_msp432p401r_ccs.obj \
+./system_msp432p401r.obj 
+
+OBJS__QUOTED += \
+"Bump.obj" \
+"Clock.obj" \
+"CortexM.obj" \
+"Lab13_Timersmain.obj" \
+"LaunchPad.obj" \
+"Motor.obj" \
+"PWM.obj" \
+"TExaS.obj" \
+"TimerA1.obj" \
+"blinker.obj" \
+"startup_msp432p401r_ccs.obj" \
+"system_msp432p401r.obj" 
+
+C_DEPS__QUOTED += \
+"Bump.d" \
+"Clock.d" \
+"CortexM.d" \
+"Lab13_Timersmain.d" \
+"LaunchPad.d" \
+"Motor.d" \
+"PWM.d" \
+"TExaS.d" \
+"TimerA1.d" \
+"blinker.d" \
+"startup_msp432p401r_ccs.d" \
+"system_msp432p401r.d" 
+
+C_SRCS__QUOTED += \
+"C:/ti/tirslk_max_1_00_02/inc/Bump.c" \
+"C:/ti/tirslk_max_1_00_02/inc/Clock.c" \
+"C:/ti/tirslk_max_1_00_02/inc/CortexM.c" \
+"../Lab13_Timersmain.c" \
+"C:/ti/tirslk_max_1_00_02/inc/LaunchPad.c" \
+"C:/ti/tirslk_max_1_00_02/inc/Motor.c" \
+"C:/ti/tirslk_max_1_00_02/inc/PWM.c" \
+"C:/ti/tirslk_max_1_00_02/inc/TExaS.c" \
+"C:/ti/tirslk_max_1_00_02/inc/TimerA1.c" \
+"C:/ti/tirslk_max_1_00_02/inc/blinker.c" \
+"../startup_msp432p401r_ccs.c" \
+"../system_msp432p401r.c" 
+
+
